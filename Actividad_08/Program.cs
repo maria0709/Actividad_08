54e1fb795ebe569d7e1a4969c0017e5abb0a24c8
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-27 00:47:27 -0300

    Entrega de la actividad 8, a pedido del docente.

---
+++ Actividad_08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_08
+{
+    class Program
+    {
+        //Solicite al usuario el ingreso de una frase y presente un mensaje indicando si alguna contiene la palabra “fin”.
+        static void Main(string[] args)
+        {
+            string frase;
+
+            Console.WriteLine("Ingrese una frase: ");
+            frase = Console.ReadLine();
+            Console.WriteLine("\r");
+
+            if (frase.ToLower().Contains("fin")) 
+             {
+                Console.WriteLine("La frase que ingreso contiene la palabra 'fin'.");
+             }
+            else
+            {
+                Console.WriteLine("La frase que ingreso no contiene la palabra 'fin'.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
